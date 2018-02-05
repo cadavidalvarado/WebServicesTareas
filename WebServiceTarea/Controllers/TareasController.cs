8e9d828e8bf25ee64d260e87b7685be2bbb68233
Carlos Alvarado <carlosalvaradofarfan@gmail.com>
2018-02-04 19:10:04 -0500

    Creación de clases bases para la prueba del CRUD

---
+++ WebServiceTarea/Controllers/TareasController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebServiceTarea.Models;
+
+namespace WebServiceTarea.Controllers
+{
+    public class TareasController : ApiController
+    {
+        public ConsultarOutBindingModel GetConsultar(ConsultarOutBindingModel argumentos)
+        {
+            return null;                
+        }
+
+        public CrearOutBindingModel PostCrear(CrearInBindingModel argumentos)
+        {
+            return null;
+        }
+
+        public ActualizarOutBindingModel PostActualizar(ActualizarInBindingModel argumentos)
+        {
+            return null;
+        }
+
+        public void PostBorrar(BorrarInBindingModel argumentos)
+        {
+            
+        }
+    }
+}
---
+++ WebServiceTarea/Models/TareasBindingModels.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServiceTarea.Models
+{
+
+    public class TareasBindingModel
+    {
+
+    }
+
+    public class ConsultarInBindingModel
+    {
+        public IEnumerable<TareasBindingModel> tareas { get; set; }
+    }
+
+    public class ConsultarOutBindingModel
+    {
+    }
+
+    public class CrearInBindingModel
+    {
+
+    }
+
+    public class CrearOutBindingModel
+    {
+
+    }
+
+    public class ActualizarOutBindingModel
+    {
+
+    }
+
+    public class ActualizarInBindingModel
+    {
+
+    }
+
+    public class BorrarInBindingModel
+    {
+
+    }
+}
